5b5429f7a432f596747e543095ff6114a863d301
湖南-李钢 <2580704698@qq.com>
2021-08-25 20:35:47 +0800

    完善代码

--- src/test/java/test/java/util/concurrent/CommonUtil.java
+++ src/test/java/test/java/util/concurrent/CommonUtil.java
@@ -3,10 +3,15 @@ package test.java.util.concurrent;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @version V1.0
@@ -24,8 +29,69 @@ public class CommonUtil {
 
     //创建线程池
     private static final ExecutorService executorService =
-            new ThreadPoolExecutor(processorsCount,processorsCount*8,60L,
-                    TimeUnit.SECONDS,new LinkedBlockingDeque<>(1024),
+            new ThreadPoolExecutor(processorsCount, processorsCount * 8, 60L,
+                    TimeUnit.SECONDS, new LinkedBlockingDeque<>(1024),
                     new ThreadFactoryBuilder().setNameFormat("calculateExecutor-%d").build(),
                     new ThreadPoolExecutor.AbortPolicy());
+
+
+    public static List<CompletableFuture<Double>> calcIntervalRateOfReturn(List<Double> rawList, int scale) {
+        if (rawList == null || rawList.isEmpty()) {
+            return null;
+        }
+        if (scale < 0) {
+            scale = 2;
+        }
+        //区间初始节点累计收益率
+        Double rawStart = rawList.get(0);
+        int finalScale = scale;
+
+        //返回future返回结果到list
+        return rawList.stream().map(t -> CompletableFuture.supplyAsync(() -> calculate(rawStart, t, finalScale), executorService)).collect(Collectors.toList());
+
+    }
+
+    private static double calculate(double rawStart, Double rawElement, int scale) {
+        try {
+            BigDecimal startBigDecimal = new BigDecimal(Double.toString(rawStart));
+            BigDecimal elementBigDecimal = new BigDecimal(Double.toString(rawElement));
+
+            BigDecimal elementAdd = elementBigDecimal.add(BigDecimal.ONE);
+            BigDecimal startAdd = startBigDecimal.add(BigDecimal.ONE);
+
+            BigDecimal calData = elementAdd.divide(startAdd, 6, BigDecimal.ROUND_HALF_UP).subtract(BigDecimal.ONE);
+
+            //右移2位，相当于乘以100
+            BigDecimal multiply = calData.movePointRight(2);
+            return multiply.setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0.0;
+    }
+
+
+    public static void main(String[] args) {
+        List<Double> collect1 = Stream.of(-0.001471, 0.005247, 0.005697, 0.010783, 0.00939, 0.012461, 0.012447,
+                0.012934, 0.013866, 0.014322, 0.015805, 0.015596, 0.014346, 0.009928).collect(Collectors.toList());
+        List<Double> collect2 = Stream.of(-0.001471, 0.005247, 0.005697, 0.010783, 0.00939, 0.012461, 0.012447,
+                0.012934, 0.013866, 0.014322, 0.015805, 0.015596, 0.014346, 0.009928).collect(Collectors.toList());
+        List<CompletableFuture<Double>> completableFutures = calcIntervalRateOfReturn(collect1, 2);
+        System.out.println("main线程---1");
+        List<Double> collect3 = completableFutures.stream().map(CompletableFuture::join).collect(Collectors.toList());
+        collect3.forEach(System.out::println);
+        System.out.println("main线程---1 --计算完毕");
+
+
+        List<CompletableFuture<Double>> completableFutures1 =
+                calcIntervalRateOfReturn(collect2, 2);
+        System.out.println("main线程---2");
+        List<Double> collect4 = completableFutures1.stream().map(CompletableFuture::join).collect(Collectors.toList());
+        collect4.forEach(System.out::println);
+        System.out.println("main线程---2 --计算完毕");
+
+        executorService.shutdown();
+
+
+    }
 }
