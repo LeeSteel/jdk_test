b9f83fb9a3b61a0f61b693aea72d55af62fa7694
李钢 <2580704698@qq.com>
2022-06-08 14:47:24 +0800

    # StreamTests 新增   过滤重复选项  测试方法

--- src/test/java/test/java/util/stream/StreamTests.java
+++ src/test/java/test/java/util/stream/StreamTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -249,6 +250,40 @@ public class StreamTests {
 
         collect.stream().forEach(System.out::println);
     }
+
+    /**
+     * 查找重复元素
+     */
+    @Test
+    public void duplicateElementsTest(){
+        List<String> list = Stream.of("A","B","C","D","E","F","G","H","I","A","B").collect(Collectors.toList());
+
+        // 获得元素出现频率的 Map，键为元素，值为元素出现的次数
+        Map<String, Integer> countMap = list.stream()
+                .collect(Collectors.toMap(e -> e, e -> 1, (a, b) -> a + b));
+        System.out.println(countMap);
+
+        List<String> duplicateKeyList = countMap
+                .entrySet().stream() // 所有 entry 对应的 Stream
+                .filter(e -> e.getValue() > 1) // 过滤出元素出现次数大于 1 的 entry
+                .map(Map.Entry::getKey)  // 获得 entry 的键（重复元素）对应的 Stream
+                .collect(Collectors.toList());
+        System.out.println(duplicateKeyList);
+
+        //简写
+         duplicateKeyList = list.stream()
+                .collect(Collectors.toMap(e -> e, e -> 1, (a, b) -> a + b))  // 获得元素出现频率的 Map，键为元素，值为元素出现的次数
+                .entrySet().stream()// 所有 entry 对应的 Stream
+                .filter(e -> e.getValue() > 1) // 过滤出元素出现次数大于 1 的 entry
+                .map(Map.Entry::getKey) // 获得 entry 的键（重复元素）对应的 Stream
+                .collect(Collectors.toList());
+
+        System.out.println(duplicateKeyList);
+
+
+    }
+
+
     private List<StreamBean> getStreamBeanList(){
         List<StreamBean> beanList =new ArrayList<>();
         StreamBean  streamBean = new StreamBean();
