6908ac2d716fd02189bf229a6de5890526a811a2
李钢 <2580704698@qq.com>
2022-06-14 18:37:52 +0800

    # BigDecimal 调整测试方法

--- src/test/java/test/java/math/BigDecimalTest.java
+++ src/test/java/test/java/math/BigDecimalTest.java
@@ -3,6 +3,7 @@ package test.java.math;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * @version V1.0
@@ -43,5 +44,40 @@ public class BigDecimalTest {
 
     }
 
+    /**
+     * 结论：
+     * BigDecimal 数值 低于指定的小数位scale时,设置 scale不生效
+     */
+    @Test
+    public void setScaleTest() {
+        // eg 1
+        double aValue = 1.000;
+        BigDecimal aBig = BigDecimal.valueOf(aValue);
+        aBig.setScale(5);
+        System.out.println("aBig scale:" + aBig.scale());
+        System.out.println("aBig value:" + aBig.toString());
 
+        // eg 2
+        double bValue = 1.123456;
+        BigDecimal bBig = BigDecimal.valueOf(bValue);
+        bBig.setScale(10, RoundingMode.HALF_DOWN);
+        System.out.println("bBig scale:" + bBig.scale());
+        System.out.println("bBig value:" + bBig.toString());
+    }
+
+
+    /**
+     * 结论：
+     * BigDecimal  double 构造函数 精度是不可控的
+     * BigDecimal  String 构造函数 精度是正确的
+     * 尽量使用 BigDecimal.valueOf 来生成 BigDecimal
+     */
+    @Test
+    public void equalsTest() {
+        double aValue = 1.0;
+        BigDecimal aBig = new  BigDecimal(aValue);
+        System.out.println(aBig.toString());
+        System.out.println(aBig.equals(BigDecimal.ONE));
+
+    }
 }
